61bd150ad96cd86f354cf4d79468a8665620b9cf
Ty Noble <tylersnoble@gmail.com>
2019-01-11 04:21:16 +0000

    added mongoose!

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-var express = require("express");
-var app = express();
+var express = require("express"),
+		app = express(),
+ bodyParser = require("body-parser"),
+   mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/Portfolio", {useNewUrlParser: true});
 const port = 3000;
-var bodyParser= require("body-parser");
-var projectList = [
+//Schemas
+var projectSchema = new mongoose.Schema({
+	name: String,
+	image: String,
+	url: String
+});
+
+var Project = mongoose.model("Project",projectSchema);
+/*var projectList = [
 		{name:"SwipOrBut",description:"This was an android app I made to test swipes versus buttons",image:""},
 		{name:"FreshBooks",description:"A java backend with a jsp frontend and derby database",image:""},
 		{name:"SleepEKoala",description:"This was an android app which set koala based alarms to wake you up",image:"https://www.petmd.com/sites/default/files/Acute-Dog-Diarrhea-47066074.jpg"}
@@ -15,7 +25,7 @@ var projectList = [
         {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
         {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
         {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"}
-	];
+	];*/
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
 
@@ -24,8 +34,12 @@ app.get("/", function(req,res){
 });
 
 app.get("/projects",function(req,res){
-	
-	res.render("projects",{projectList:projectList});
+	Project.find({},function(err,allProjects){
+		if(err)
+			console.log("It messed up at retrieving projects.");
+		else
+			res.render("projects",{projects:allProjects});
+			})
 });
 
 app.post("/projects", function (req,res) {
@@ -33,7 +47,13 @@ app.post("/projects", function (req,res) {
 	var name = req.body.name;
 	var image = req.body.image;
 	var newProject= {name:name, image:image};
-	projectList.push(newProject);
+	
+	Project.create(newProject,function(err,newlycreated){
+		if(err)
+			alert("Cant be blank project bud");
+		else
+			res.redirect("projects");
+	})
 	res.redirect("/projects");
 
 	//get data from from and add to projectList
@@ -42,6 +62,12 @@ app.post("/projects", function (req,res) {
 
 app.get("/projects/new",function(req,res){
 	res.render("new.ejs");
-})
+});
 
-app.listen(port, () => console.log(`Portfolio initialized on port ${port}!`));
+//Use for local machine
+//app.listen(port, () => console.log(`Portfolio initialized on port ${port}!`));
+
+//Use for cloud9
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("The Portfolio Server Has Started!");
+});
