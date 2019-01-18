bf24669dac2b27c535ae83507880c22256de447c
Ty Noble <tylersnoble@gmail.com>
2019-01-18 20:19:34 +0000

    so now it has comments but I need to make it so you can add new ones. That way I can talk about myself and make my portfolio social

--- index.js
+++ index.js
@@ -1,34 +1,16 @@
 var express = require("express"),
 		app = express(),
  bodyParser = require("body-parser"),
-   mongoose = require("mongoose");
+   mongoose = require("mongoose"),
+   Project = require("./models/project"),
+   seedDB = require("./seeds")
+   ;
+   
 mongoose.connect("mongodb://localhost:27017/Portfolio", {useNewUrlParser: true});
 const port = 3000;
-//Schemas
-var projectSchema = new mongoose.Schema({
-	name: String,
-	image: String,
-	url: String,
-	description:String
-});
-
-var Project = mongoose.model("Project",projectSchema);
-/*var projectList = [
-		{name:"SwipOrBut",description:"This was an android app I made to test swipes versus buttons",image:""},
-		{name:"FreshBooks",description:"A java backend with a jsp frontend and derby database",image:""},
-		{name:"SleepEKoala",description:"This was an android app which set koala based alarms to wake you up",image:"https://www.petmd.com/sites/default/files/Acute-Dog-Diarrhea-47066074.jpg"}
-		,{name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
-        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
-        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"}
-	];*/
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
+seedDB();
 
 app.get("/", function(req,res){
 	res.render("landing");
@@ -68,7 +50,7 @@ app.get("/projects/new",function(req,res){
 //show
 app.get("/projects/:id",function(req, res) {
    //find project with id
-   Project.findById(req.params.id, function(err,foundProject){
+   Project.findById(req.params.id).populate("comments").exec(function(err,foundProject){
    	if(err)
    		console.log("project find didnt work");
    	else
---
+++ seeds.js
@@ -0,0 +1,44 @@
+var mongoose = require("mongoose");
+var Project = require("./models/project");
+var Comment = require("./models/comment");
+var data=[
+		{name:"SwipOrBut",description:"This was an android app I made to test swipes versus buttons",image:"https://amp.businessinsider.com/images/57179035910584e73c8bda62-750-563.jpg"},
+		{name:"FreshBooks",description:"A java backend with a jsp frontend and derby database",image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRASrpGGY1uAe5h09cv0MGi58b31DjizbGpI3cDmbsRuXfv45Ew"},
+		{name:"SleepEKoala",description:"This was an android app which set koala based alarms to wake you up",image:"https://www.petmd.com/sites/default/files/Acute-Dog-Diarrhea-47066074.jpg"}
+		]
+
+function seedDB(){
+    //removes projects
+    Project.remove({},function(err){
+        if(err){
+            console.log(err);
+        }
+        console.log("removed projects"); //adds a few projects
+    data.forEach(function(seed){
+        Project.create(seed,function(err,project){
+            if(err)
+                console.log(err);
+            else
+                console.log("added Campground");
+            
+            //create a comment
+            Comment.create({
+                text:"This project is amazing this guy should be hired",
+                author:"Old Boss"
+            },function(err,comment){
+                if(err)
+                    console.log("No comment was added it errored");
+                else{
+                project.comments.push(comment);
+                project.save();
+                console.log("created new comment");
+                    
+                }
+            });
+                    });
+    });
+   
+    })
+}
+ 
+ module.exports = seedDB;
